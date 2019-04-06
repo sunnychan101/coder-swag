da40194440b7623d7bf3408be51326a95c16efde
Sunny Chan <kschan.sunny@gmail.com>
2019-04-06 18:22:51 +0800

    first commit

---
+++ coder-swag/coder-swag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag
+//
+//  Created by Kwok San CHAN on 6/4/2019.
+//  Copyright © 2019 Sunny Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
